6b1fac19b5c78eb241ad8f7e6f6d9ebf358139ae
matheuswilliam95 <matheus.william95@live.com>
2025-02-15 12:05:19 -0300

    Remove error reporting settings from login.php for production readiness

--- files/pages/login.php
+++ files/pages/login.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once __DIR__ . '/../includes/config.php';
 require_once __DIR__ . '/../includes/db.php';
 require_once __DIR__ . '/../includes/auth.php';
