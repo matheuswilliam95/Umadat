1d9818d27c35131e65f0545bfca121b628c75e28
matheuswilliam95 <matheus.william95@live.com>
2025-02-25 20:11:52 -0300

    Fix logo image path in login page for dynamic loading

--- files/pages/login.php
+++ files/pages/login.php
@@ -44,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
 
     <div class="logo_img_div">
-        <img src="/../public/img/system_logo.png">
+        <img src="<?php echo PASTA_BASE; ?>public/img/system_logo.png">
     </div>
 
     <div class="main_container">
