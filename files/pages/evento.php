db5050ba132bf50ea8520967253505d549f45147
matheuswilliam95 <matheus.william95@live.com>
2025-03-01 08:30:53 -0300

    Refactor share button implementation and improve layout

--- files/pages/evento.php
+++ files/pages/evento.php
@@ -121,54 +121,53 @@ $relatedEvents = getRelatedEvents($eventId);
 
                     <!-- Botão para compartilhar link do evento -->
                     <div style="text-align: center;">
+                        <button id="compartilhar-btn" class="botoes_link_evento" onclick="compartilharEvento()">
+                            <img class="icon_evento" src="<?php echo PASTA_BASE; ?>public/img/share_icon.png"
+                                alt="Compartilhar">
+                        </button>
+                        <br><small>Compartilhar</small>
                     </div>
-                    <button id="compartilhar-btn" class="botoes_link_evento" onclick="compartilharEvento()">
-                        <img class="icon_evento" src="<?php echo PASTA_BASE; ?>public/img/share_icon.png"
-                            alt="Compartilhar">
-                    </button>
-                    <br><small>Compartilhar</small>
+
+                    <script>
+                        function compartilharEvento() {
+                            const url = window.location.href;
+                            if (navigator.share) {
+                                navigator.share({
+                                    title: '<?php echo htmlspecialchars($evento['titulo']); ?>',
+                                    text: 'Confira este evento:',
+                                    url: url
+                                }).catch(console.error);
+                            } else {
+                                prompt('Copie o link do evento:', url);
+                            }
+                        }
+                    </script>
+
+                    <!-- Local -->
+                    <p>
+                        <?php
+                        $local = $evento['local'] ?? 'N/A';
+                        if ($local !== 'N/A') {
+                            echo '<div style="text-align: center;">';
+                            echo '<a class="botoes_link_evento" href="https://www.google.com/maps/search/?api=1&query=' . urlencode($local) . '" target="_blank"><img class="icon_evento" src="' . PASTA_BASE . 'public/img/place_icon.png" alt="Local"></a>';
+                            echo '<br><small>Ver no mapa</small>';
+                            echo '</div>';
+                        } else {
+                            echo 'N/A';
+                        }
+                        ?>
+                    </p>
                 </div>
 
-                <script>
-                    function compartilharEvento() {
-                        const url = window.location.href;
-                        if (navigator.share) {
-                            navigator.share({
-                                title: '<?php echo htmlspecialchars($evento['titulo']); ?>',
-                                text: 'Confira este evento:',
-                                url: url
-                            }).catch(console.error);
-                        } else {
-                            prompt('Copie o link do evento:', url);
-                        }
-                    }
-                </script>
-
-                <!-- Local -->
-                <p>
-                    <?php
-                    $local = $evento['local'] ?? 'N/A';
-                    if ($local !== 'N/A') {
-                        echo '<div style="text-align: center;">';
-                        echo '<a class="botoes_link_evento" href="https://www.google.com/maps/search/?api=1&query=' . urlencode($local) . '" target="_blank"><img class="icon_evento" src="' . PASTA_BASE . 'public/img/place_icon.png" alt="Local"></a>';
-                        echo '<br><small>Ver no mapa</small>';
-                        echo '</div>';
-                    } else {
-                        echo 'N/A';
-                    }
-                    ?>
-                </p>
+                <h3>Eventos Relacionados</h3>
+                <ul>
+                    <?php foreach ($relatedEvents as $relEvent): ?>
+                        <li><a href="evento.php?id=<?php echo $relEvent['id']; ?>">
+                                <?php echo htmlspecialchars($relEvent['titulo']); ?> </a></li>
+                    <?php endforeach; ?>
+                </ul>
             </div>
-
-            <h3>Eventos Relacionados</h3>
-            <ul>
-                <?php foreach ($relatedEvents as $relEvent): ?>
-                    <li><a href="evento.php?id=<?php echo $relEvent['id']; ?>">
-                            <?php echo htmlspecialchars($relEvent['titulo']); ?> </a></li>
-                <?php endforeach; ?>
-            </ul>
         </div>
-    </div>
     </div>
 </body>
 <footer>
