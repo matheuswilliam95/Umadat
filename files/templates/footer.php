83737683d177ecea84223f0b64846efb2b6b7ea9
matheuswilliam95 <matheus.william95@live.com>
2025-03-01 19:20:16 -0300

    Remove social media icons from footer for a cleaner design

--- files/templates/footer.php
+++ files/templates/footer.php
@@ -6,12 +6,7 @@ require_once __DIR__ . '/../includes/config.php';
         <a href="<?php echo BASE_URL; ?>contato.php">Contato</a>
         <a href="<?php echo BASE_URL; ?>privacidade.php">Política de Privacidade</a>
     </div>
-    <div class="footer_social">
-        <a href="#" class="social-icon">📘</a>
-        <a href="#" class="social-icon">🐦</a>
-        <a href="instagram.com.br" class="social-icon"><img src="<?php echo PASTA_BASE; ?>public/img/instagram_icon.png"
-                alt="Instagram" /></a>
-    </div>
+
     <div class="footer_copy">
         <p>&copy; <?php echo date('Y'); ?> <?php echo SITE_NAME; ?>. Todos os direitos reservados.</p>
         <p>Desenvolvido com ❤️ para nossa Igreja</p>
