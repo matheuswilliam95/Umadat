72c84889ee451e5c086b0d3d0d31b597d17054c7
matheuswilliam95 <matheus.william95@live.com>
2025-03-01 16:34:44 -0300

    Add validation for geocoding query parameter in proxy

--- files/includes/proxy.php
+++ files/includes/proxy.php
@@ -2,8 +2,11 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$query = urlencode($_GET['q']);
-$url = "https://nominatim.openstreetmap.org/search?q=$query&limit=5&format=json&addressdetails=1";
-
-echo file_get_contents($url);
+if (isset($_GET['q']) && !empty($_GET['q'])) {
+    $query = urlencode($_GET['q']);
+    $url = "https://nominatim.openstreetmap.org/search?q=$query&limit=5&format=json&addressdetails=1";
+    echo file_get_contents($url);
+} else {
+    echo json_encode([]);
+}
 ?>
