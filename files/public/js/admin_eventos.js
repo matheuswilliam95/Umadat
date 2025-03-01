0b8d44534d7e2b40293eedce45166a9bf4e2aed8
matheuswilliam95 <matheus.william95@live.com>
2025-03-01 16:44:30 -0300

    Enhance autocomplete functionality with error handling and logging

--- files/public/js/admin_eventos.js
+++ files/public/js/admin_eventos.js
@@ -1,32 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+    initAutocomplete();
+});
+
 function initAutocomplete() {
+    console.log("Inicializando auto completar.");
     const input = document.getElementById('local');
     const suggestionsContainer = document.getElementById('suggestions');
-    input.addEventListener('input', function () {
+
+    if (!input || !suggestionsContainer) {
+        console.error("Elemento de input ou container de sugestões não encontrado.");
+        return;
+    }
+
+    input.addEventListener('input', () => {
         const query = input.value;
+        console.log(`Valor do input: ${query}`);
+
         if (query.length > 2) {
-            fetch(`../includes/proxy.php?q=${encodeURIComponent(query)}`)
+            const url = `../includes/proxy.php?q=${encodeURIComponent(query)}`;
+            console.log(`Fazendo fetch para: ${url}`);
+
+            fetch(url)
                 .then(response => {
                     if (!response.ok) {
-                        throw new Error('Network response was not ok');
+                        throw new Error('Resposta da rede não ok');
                     }
                     return response.json();
                 })
                 .then(results => {
+                    console.log("Resultados obtidos:", results);
                     suggestionsContainer.innerHTML = '';
+
                     if (results.length > 0) {
                         results.forEach(result => {
                             const suggestionItem = document.createElement('div');
                             suggestionItem.className = 'suggestion-item';
                             suggestionItem.textContent = result.display_name;
-                            suggestionItem.addEventListener('click', function () {
+
+                            suggestionItem.addEventListener('click', () => {
                                 input.value = result.display_name;
                                 suggestionsContainer.innerHTML = '';
                             });
+
                             suggestionsContainer.appendChild(suggestionItem);
                         });
+                    } else {
+                        console.log("Nenhuma sugestão encontrada.");
                     }
                 })
-                .catch(error => console.error('Error:', error));
+                .catch(error => console.error("Erro durante a requisição:", error));
         } else {
             suggestionsContainer.innerHTML = '';
         }
