86e29fe8c140128c23093f86012189ab99973fad
matheuswilliam95 <matheus.william95@live.com>
2025-03-01 16:44:07 -0300

    Add User-Agent header to geocoding requests in proxy

--- files/includes/proxy.php
+++ files/includes/proxy.php
@@ -5,7 +5,15 @@ header("Content-Type: application/json");
 if (isset($_GET['q']) && !empty($_GET['q'])) {
     $query = urlencode($_GET['q']);
     $url = "https://nominatim.openstreetmap.org/search?q=$query&limit=5&format=json&addressdetails=1";
-    $response = file_get_contents($url);
+
+    $opts = [
+        'http' => [
+            'method' => 'GET',
+            'header' => "User-Agent: MeuApp/1.0 (seuemail@dominio.com)\r\n"
+        ]
+    ];
+    $context = stream_context_create($opts);
+    $response = file_get_contents($url, false, $context);
     if ($response === FALSE) {
         echo json_encode([]);
     } else {
